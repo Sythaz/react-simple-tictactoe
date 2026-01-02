d077c7cacf46c73c3417f70e099a227563327fd3
Sythaz <syafiqaldiansyah@gmail.com>
2026-01-02 17:01:44 +0700

    style: improve layout and styling for game container and history section with CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import Board from './components/board'
+import Board from "./components/board";
 import { calculateWinner } from "./utils/gameLogic";
 
 export default function App() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [step, setStep] = useState(0);
 
-  const currentSquares = history[step]; 
+  const currentSquares = history[step];
   const isXNext = step % 2 === 0;
 
   const winner = calculateWinner(currentSquares);
@@ -24,7 +24,7 @@ export default function App() {
     if (currentSquares[i] || winner) return;
 
     const nextHistory = history.slice(0, step + 1);
-    
+
     const nextSquares = currentSquares.slice();
     nextSquares[i] = isXNext ? "X" : "O";
 
@@ -47,12 +47,22 @@ export default function App() {
 
   return (
     <>
-      <div className="status">{status}</div>
-      <div className="game">
-        <Board values={currentSquares} onClick={handleClick} />
-      </div>
-      <div className="history">
-        <ol>{moves}</ol>
+      <div className="root">
+        <div className="status">{status}</div>
+
+        <div className="game-container">
+          
+          <div className="game">
+            <Board values={currentSquares} onClick={handleClick} />
+          </div>
+
+          <div className="history">
+            <h3>History: </h3>
+            <ol>{moves}</ol>
+          </div>
+        
+        </div>
+
       </div>
     </>
   );
